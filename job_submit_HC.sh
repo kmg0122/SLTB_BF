4b375c4c0947d661998b0e5f492481a0566aad96
Mingang Kim <kmg0122@tinkercliffs2.cm.cluster>
2026-04-21 15:31:21 -0400

    Apr 21 3rd

--- job_submit_HC.sh
+++ job_submit_HC.sh
@@ -6,11 +6,13 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --array=1-9%3
+#SBATCH --array=1-2
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=kmg0122@vt.edu
 #SBATCH -o logs/%x-%A_%a.out
 #SBATCH -e logs/%x-%A_%a.err
+
+mkdir -p logs
 
 module load R-bundle-CRAN/2025.11-foss-2025b
 
@@ -18,16 +20,10 @@ export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 case "${SLURM_ARRAY_TASK_ID}" in
-  1) n=50  tau_name="small"  tau_sd=0.1 ;;
-  2) n=50  tau_name="medium" tau_sd=0.4472136 ;;
-  3) n=50  tau_name="big"    tau_sd=0.7071068 ;;
-  4) n=100 tau_name="small"  tau_sd=0.1;;
-  5) n=100 tau_name="medium" tau_sd=0.4472136 ;;
-  6) n=100 tau_name="big"    tau_sd=0.7071068 ;;
-  7) n=500 tau_name="small"  tau_sd=0.1 ;;
-  8) n=500 tau_name="medium" tau_sd=0.4472136 ;;
-  9) n=500 tau_name="big"    tau_sd=0.7071068 ;;
+  1) n=500 tau_name="medium" tau_sd=0.4472136 ;;
+  2) n=500 tau_name="big"    tau_sd=0.7071068 ;;
 esac
 
 echo "Starting task ${SLURM_ARRAY_TASK_ID}: n=${n}, tau=${tau_name}"
-Rscript simulation_HC.R "${n}" "${tau_name}" "${tau_sd}"
+Rscript --vanilla simulation_HC.R "${n}" "${tau_name}" "${tau_sd}"
+echo "Finished task ${SLURM_ARRAY_TASK_ID}"
